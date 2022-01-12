4b91c6d4017cb8298c391125d3de570640ae1844
Alex Dean <alex@zanconsulting.com>
2022-01-11 17:52:21 -0700

    Initial work on Zan.common.String.from and fromBoolean

--- src/String.js
+++ src/String.js
@@ -19,4 +19,47 @@ Ext.define('Zan.common.String', {
 
         return str;
     },
+
+    /**
+     * Returns a string by automatically detecting its value and calling one of the other from* methods
+     *
+     * This method supports the following options (in addition to options supported by other from* methods):
+     *      nullValueString - the string to return when a null or undefined value is passed
+     */
+    from: function(value, options) {
+        options = options || {};
+        options = Ext.applyIf(options, {
+            nullValueString: '',
+        });
+
+        if (value === null || value === undefined) return options.nullValueString;
+
+        if (Ext.isString(value)) {
+            return value;
+        }
+
+        if (Ext.isBoolean(value)) {
+            return this.fromBoolean(value, options);
+        }
+    },
+
+    /**
+     * Converts a Boolean to a String
+     *
+     * Supported options:
+     *      trueText: 'Yes'
+     *      falseText: 'No'
+     *      nullText: ''
+     */
+    fromBoolean: function(value, options) {
+        options = Ext.applyIf(options, {
+            trueText: 'Yes',
+            falseText: 'No',
+            nullText: '',
+        });
+
+        if (value === null || value === undefined) return options.nullText;
+        if (value === true) return options.trueText;
+        if (value === false) return options.falseText;
+    }
 });
